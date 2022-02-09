c47886e845aeb0b420492c103db4e8c415b59b76
artur-ws <wysocki.a97@gmail.com>
2022-02-09 18:43:03 +0100

    Perceptron class added with metods: <check_data> and <output> whichacts similar to numPy.dot

--- model.py
+++ model.py
@@ -1,8 +1,33 @@
-import numpy
+import numpy as np
 import random
 
 
-class Module():
-    def __init__(self, arg1):
-        self.arg1=arg1
-        
+class Perceptron():
+
+    def __init__(self, input_list, weight_list, bias=0):
+        self.input_list  = input_list
+        self.weight_list = weight_list
+        self.bias        = bias
+        # Check whether there is same number of inputs and weights
+        self.check_data()
+
+    def check_data(self):
+
+        if len(self.input_list) != len(self.weight_list):
+            print(self)
+            raise ValueError('input_list and weight_list must have the same number of elements!')
+
+    def output(self):
+
+        output_value = self.bias
+        for input_value, weight in zip(self.input_list, self.weight_list):
+            output_value += input_value * weight
+        return output_value
+
+
+input_list = (1, 2, 3, 4)
+weights = (0.2, 0.6, -0.3, 0.9)
+bias = 0
+x = Perceptron(input_list, weights, bias)
+
+print(x.output())
