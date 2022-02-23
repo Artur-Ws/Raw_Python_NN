1bf330b3b53e4a34a17947e885e3536585b5567b
Artur-Ws <wysywysy97@gmail.com>
2022-02-23 11:13:33 +0100

    Fixed init and forward method in model1.py

--- model1.py
+++ model1.py
@@ -4,11 +4,11 @@ import numpy as np
 class LayerDense:
     """A class for dense layer of neurons instantiate"""
 
-    def __init__(self, inputs, neurons):
-        self.inputs = inputs
-        self.neurons = neurons
-        self.weights = np.random.randn(inputs, neurons) * 0.01
-        self.biases = np.zeros(1, neurons)
+    def __init__(self, n_inputs, n_neurons):
+        self.inputs = n_inputs
+        self.neurons = n_neurons
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
+        self.biases = np.zeros(1, n_neurons)
 
-    def forward(self, ):
-        pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
