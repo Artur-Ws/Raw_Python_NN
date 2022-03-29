5dca2cadc2c5af70617199acfc16391dd3c87557
Artur-Ws <wysywysy97@gmail.com>
2022-03-29 17:15:28 +0200

    Added some stuff to data_visualization.py about pandas data processing

--- data_visualization.py
+++ data_visualization.py
@@ -5,8 +5,28 @@ import pandas as pd
 
 dataset = pd.read_csv("datasets/Unicorn_Companies.csv")
 
+sep = ' - '
+
 # Print list of features
 print(dataset.columns)
+#
+# # Print one specific feature
+# print(dataset["Date Joined"])
+#
+# # Print list of all elements for specific feature one by one
+# for col in dataset.columns:
+#     print(dataset[col])
+#
+# # Print which year the oldest company on the list was founded
+# print(dataset['Founded Year'].min())
+#
+# # Print general info about dataset
+# print(dataset.describe())
+#
+# # Print info about data in dataset
+# print(dataset.info())
 
-# Print one specific feature
-print(dataset["Date Joined"])
+# Print specific info about Companies from specific country
+for index, row in dataset.iterrows():
+    if row['Country'] == 'Germany':
+        print(index, sep, row['Founded Year'], sep, row['Valuation ($B)'], sep, row['Company'], sep, row['Industry'])
